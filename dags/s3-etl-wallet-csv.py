f19dde332412a42edbc76b028defdfd91aa5cc4e
AfonSir <afonsir17@gmail.com>
2021-06-12 19:39:02 -0300

    feat: add DAG for wallet etl

--- dags/s3-etl-wallet-csv.py
+++ dags/s3-etl-wallet-csv.py
@@ -19,17 +19,17 @@ MINIO = getenv("MINIO", "minio:9000")
 ACCESS_KEY = getenv("ACCESS_KEY", "airflow_access_key")
 SECRET_ACCESS = getenv("SECRET_ACCESS", "airflow_secret_key")
 POSTGRESDB = getenv("POSTGRESDB", "postgresql://datawarehouse:datawarehouse@postgres-dw:5432/datawarehouse")
-BUSINESS_ORIGINAL_FILE_LOCATION = getenv("BUSINESS_ORIGINAL_FILE_LOCATION", "business/yelp_business.json")
+BUSINESS_ORIGINAL_FILE_LOCATION = getenv("BUSINESS_ORIGINAL_FILE_LOCATION", "cyrela/wallet-data.csv")
 LANDING_ZONE = getenv("LANDING_ZONE", "landing")
 PROCESSING_ZONE = getenv("PROCESSING_ZONE", "processing")
 CURATED_ZONE = getenv("CURATED_ZONE", "curated")
-CURATED_BUSINESS_CSV_FILE = getenv("CURATED_BUSINESS_CSV_FILE", "business/yelp_business.csv")
+CURATED_BUSINESS_CSV_FILE = getenv("CURATED_BUSINESS_CSV_FILE", "cyrela/wallet-data.csv")
 # [END env_variables]
 
 # [START default_args]
 default_args = {
     'owner': 'afonso de sousa costa',
-    'start_date': datetime(2021, 6, 9),
+    'start_date': datetime(2021, 6, 12),
     'depends_on_past': False,
     'email': ['afonsir21@gmail.com'],
     'email_on_failure': False,
@@ -40,7 +40,7 @@ default_args = {
 
 # [START instantiate_dag]
 dag = DAG(
-    's3-etl-business-json',
+    's3-etl-wallet-csv',
     default_args=default_args,
     schedule_interval='@daily',
     tags=['development', 's3', 'sensor', 'minio', 'python', 'postgresdb'])
@@ -48,11 +48,15 @@ dag = DAG(
 
 
 # [START functions]
-def read_business_json_data(ti):
+def format_date_to_postgres(string_date):
+    if isinstance(string_date, str):
+        return datetime.strptime(string_date, '%d/%m/%Y').strftime('%Y-%m-%d')
+
+def parse_wallet_data(ti):
     # airflow feature that allows the exchange messages for sharing states
     # defined by a key and value, to get access use the task id
-    # ['business/yelp_business.json']
     get_xcom_file_name = ti.xcom_pull(task_ids=['list_file_s3'])
+
     print(get_xcom_file_name)
     print(type(get_xcom_file_name))
 
@@ -63,21 +67,44 @@ def read_business_json_data(ti):
     # processing engine access processing bucket [always]
     # empty if nothing to process
     # reading xcom from list s3 task
-    obj_business = client.get_object(
+    obj_wallet = client.get_object(
         PROCESSING_ZONE,
         get_xcom_file_name[0][0],
     )
 
-    # read json file using pandas
-    # select and output only 10 rows
-    # convert to dictionary
-    # reduce amount = .head(10000) [dataframe]
-    df_business = pd.read_json(obj_business, lines=True)
-    selected_data = df_business[["business_id", "name", "city", "state", "stars", "review_count"]].head(5000)
-    selected_data.to_dict('records')
+    # read csv file using pandas
+    df_header = [
+	'empresa',
+	'marca',
+	'empreendimento',
+	'cliente',
+	'regional',
+	'obra',
+	'bloco',
+	'unidade',
+	'dt_venda',
+	'dt_chaves',
+	'carteira_sd_gerencial',
+	'saldo_devedor',
+	'data_base',
+	'total_atraso',
+	'faixa_de_atraso',
+	'dias_atraso',
+	'valor_pago_atualizado',
+	'valor_pago',
+	'status',
+	'dt_reneg',
+	'descosn',
+	'vaga',
+	'vgv'
+	]
+    df_wallet = pd.read_csv(obj_wallet, header=1, names=df_header)
+
+    for date_column in ['dt_venda', 'dt_chaves', 'data_base', 'dt_reneg']:
+        df_wallet[date_column] = df_wallet[date_column].apply(format_date_to_postgres)
 
     # dataframe to csv - encode and buffer bytes
-    csv_bytes = selected_data.to_csv(header=True, index=False).encode('utf-8')
+    csv_bytes = df_wallet.to_csv(header=True, index=False).encode('utf-8')
     csv_buffer = BytesIO(csv_bytes)
 
     # writing into minio storage [curated zone]
@@ -88,22 +115,22 @@ def read_business_json_data(ti):
         length=len(csv_bytes),
         content_type='application/csv')
 
-
-def write_business_dt_postgres():
+def write_wallet_dt_postgres():
     # set up connectivity with minio storage
     client = Minio(MINIO, ACCESS_KEY, SECRET_ACCESS, secure=False)
 
     # download file from bucket
-    obj_business = client.get_object(CURATED_ZONE, CURATED_BUSINESS_CSV_FILE)
+    obj_wallet = client.get_object(CURATED_ZONE, CURATED_BUSINESS_CSV_FILE)
 
     # read file from curated zone
-    df_business = pd.read_csv(obj_business)
+    df_wallet = pd.read_csv(obj_wallet)
 
     # insert pandas data frame into postgres db [sqlalchemy]
     # connect into postgres and ingest data
     #  adding method multi
     postgres_engine = create_engine(POSTGRESDB)
-    df_business.to_sql('business', postgres_engine, if_exists='append', index=False, chunksize=10)
+
+    df_wallet.to_sql('wallet', postgres_engine, if_exists='append', index=False, chunksize=10)
 # [END functions]
 
 
@@ -112,7 +139,7 @@ def write_business_dt_postgres():
 verify_file_existence_landing = S3KeySensor(
     task_id='verify_file_existence_landing',
     bucket_name=LANDING_ZONE,
-    bucket_key='business/*.json',
+    bucket_key='cyrela/*.csv',
     wildcard_match=True,
     timeout=18 * 60 * 60,
     poke_interval=120,
@@ -123,7 +150,7 @@ verify_file_existence_landing = S3KeySensor(
 list_file_s3 = S3ListOperator(
     task_id='list_file_s3',
     bucket=LANDING_ZONE,
-    prefix='business/',
+    prefix='cyrela/',
     delimiter='/',
     aws_conn_id='minio',
     dag=dag)
@@ -147,9 +174,9 @@ delete_s3_file_landing_zone = S3DeleteObjectsOperator(
     dag=dag)
 
 # apply transformation [python function]
-process_business_data = PythonOperator(
-    task_id='process_business_data',
-    python_callable=read_business_json_data,
+process_wallet_data = PythonOperator(
+    task_id='process_wallet_data',
+    python_callable=parse_wallet_data,
     dag=dag)
 
 # delete file from processed zone
@@ -160,38 +187,49 @@ delete_s3_file_processed_zone = S3DeleteObjectsOperator(
     aws_conn_id='minio',
     dag=dag)
 
-# delete table to perform full load
-drop_postgres_tb = PostgresOperator(
-    task_id='drop_postgres_tb',
-    postgres_conn_id='postgresdb_psql',
-    sql=""" DROP TABLE IF EXISTS business; """,
-    dag=dag)
-
 # create table on postgres [if not exists]
 create_postgres_tb = PostgresOperator(
     task_id='create_postgres_tb',
     postgres_conn_id='postgresdb_psql',
     sql="""
-        CREATE TABLE IF NOT EXISTS business 
+        CREATE TABLE IF NOT EXISTS wallet 
         (
-            id SERIAL PRIMARY KEY, 
-            business_id VARCHAR NOT NULL, 
-            name VARCHAR NULL, 
-            city VARCHAR NULL, 
-            state VARCHAR NULL,
-            stars NUMERIC NULL,
-            review_count INTEGER NULL
-        );""",
+            id                    SERIAL PRIMARY KEY, 
+            empresa               VARCHAR NULL,
+            marca                 VARCHAR NULL,
+            empreendimento        VARCHAR NULL,
+            cliente               VARCHAR NULL,
+            regional              VARCHAR NULL,
+            obra                  VARCHAR NULL,
+            bloco                 VARCHAR NULL,
+            unidade               VARCHAR NULL,
+            dt_venda              DATE NULL,
+            dt_chaves             DATE NULL,
+            carteira_sd_gerencial VARCHAR NULL,
+            saldo_devedor         NUMERIC NULL,
+            data_base             DATE NULL,
+            total_atraso          NUMERIC NULL,
+            faixa_de_atraso       VARCHAR NULL,
+            dias_atraso           VARCHAR NULL,
+            valor_pago_atualizado NUMERIC NULL,
+            valor_pago            NUMERIC NULL,
+            status                VARCHAR NULL,
+            dt_reneg              VARCHAR NULL,
+            descosn               VARCHAR NULL,
+            vaga                  VARCHAR NULL,
+            vgv                   NUMERIC NULL
+        );
+        """,
     dag=dag)
 
 # write data into postgres database ingestion
 # of [~ 2 million] rows on table
-write_business_dt_postgresdb = PythonOperator(
-    task_id='write_business_dt_postgresdb',
-    python_callable=write_business_dt_postgres,
+write_wallet_dt_postgresdb = PythonOperator(
+    task_id='write_wallet_dt_postgresdb',
+    python_callable=write_wallet_dt_postgres,
     dag=dag)
 # [END set_tasks]
 
 # [START task_sequence]
-verify_file_existence_landing >> list_file_s3 >> copy_s3_file_processed_zone >> delete_s3_file_landing_zone >> process_business_data >> delete_s3_file_processed_zone >> drop_postgres_tb >> create_postgres_tb >> write_business_dt_postgresdb
+verify_file_existence_landing >> list_file_s3 >> copy_s3_file_processed_zone >> delete_s3_file_landing_zone >> process_wallet_data >> delete_s3_file_processed_zone >> create_postgres_tb >> write_wallet_dt_postgresdb
 # [END task_sequence]
